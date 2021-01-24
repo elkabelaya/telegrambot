b5dc18aa5453c8d01c78fb240e08e9449d8328d1
lkukuruku <l.kukuruku@gmail.com>
2021-01-24 07:43:01 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from app import app
+import git
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/update_repo/', methods=["POST"])
+def update(request):
+    repo = git.Repo("elkabelaya.pythonanywhere.com/")
+    origin = repo.remotes.origin
+    origin.pull()
+    return "OK"
