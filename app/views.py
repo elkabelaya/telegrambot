c97385c9dadcadb1c3d2491b5584f9e8b54bd8a4
lkukuruku <l.kukuruku@gmail.com>
2021-01-24 13:25:18 +0300

    fix webhook secret

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
-from settings import WEBHOOK_SECRET_KEY
-from utils import is_valid_signature
+from app import utils
+from app import settings
 import git
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -11,7 +12,7 @@ def index():
 @app.route('/update_repo/', methods=["POST"])
 def update():
     x_hub_signature = request.headers.get('X-Hub-Signature')
-    if is_valid_signature(x_hub_signature, request.data, WEBHOOK_SECRET_KEY):
+    if utils.is_valid_signature(x_hub_signature, request.data, settings.WEBHOOK_SECRET_KEY):
         repo = git.Git('mysite')
         repo.pull('origin', 'main')
         return "OK"
